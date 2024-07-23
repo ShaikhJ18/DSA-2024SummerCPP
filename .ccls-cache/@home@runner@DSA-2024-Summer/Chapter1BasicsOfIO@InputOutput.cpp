4960b5ed2d9d616d56631121ca4f8a4e7c9429d9
Jawad Shaikh <J.shk1980@gmail.com>
2024-07-23 20:29:30 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@DSA-2024-Summer/Chapter1BasicsOfIO@InputOutput.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main(){
+  //declare variables a , b ,x
+  int a,b;
+  string x;
+  //read input through cin stream
+  cin >> a >> b >> x;
+  //output the values of a , b , x
+  cout << a << b << x << "\n";
+  //This type of input out works if there is a space between the values
+
+  //Output Works as follows
+  //declare variables 
+  int c = 123, d = 456;
+  string name = "Timmy Turner";
+  //output the values of c , d , name
+  cout << c << " " << d << " " << name << "\n";
+  
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@DSA-2024-Summer/Chapter1BasicsOfIO@basic.cpp
@@ -0,0 +1,16 @@
+//include the header files with common data structures
+#include<bits/stdc++.h>
+
+//wont need to add std to all the common IO functions
+using namespace std;
+
+//main function where execution enters
+int main(){
+  cout << "Hello World";
+  return 0;
+}
+
+//this beginner syntax is for competitive programming standards
+
+//compile the code using 
+//g++ -std=c++11 -O2 -Wall urFile.cpp -o urFile
