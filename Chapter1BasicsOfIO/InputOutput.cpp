e46b86b1f81d4fdfaa9a0cfbd02164f3ccca4e55
Jawad Shaikh <J.shk1980@gmail.com>
2024-07-23 20:51:38 +0000

    began understanding how numbers are implemented in c++

--- Chapter1BasicsOfIO/InputOutput.cpp
+++ Chapter1BasicsOfIO/InputOutput.cpp
@@ -5,11 +5,16 @@ using namespace std;
 
 
 int main(){
+  //These Two lines are for competitive programming standards
+  //they make the code more efficient
+  ios::sync_with_stdio(0);
+  cin.tie(0);
   //declare variables a , b ,x
   int a,b;
   string x;
   //read input through cin stream
   cin >> a >> b >> x;
+  //\n works faster than endl because it doesnt need to flush the buffer
   //output the values of a , b , x
   cout << a << b << x << "\n";
   //This type of input out works if there is a space between the values
@@ -20,6 +25,27 @@ int main(){
   string name = "Timmy Turner";
   //output the values of c , d , name
   cout << c << " " << d << " " << name << "\n";
+
+  //Sometimes you need to read a whole line of text with newline characters
+  //string Line;
+  //getLine function takes the whole line as input
+  //getline(cin,Line);
+  //cout << "The line is: " << Line << "\n";
   
+  //Alternatively you can use the C syntax scanf() printf()
+  int e,f;
+  scanf("%d %d",&e,&f);
+  printf("%d %d",e,f);
+
+  //if the input size is unnknown use the following loop
+  // while (cin >> x){
+    //code
+  //}
+
+  // file handling can be done with the following
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  //After this, the program reads the input from the file ”input.txt” and writes the
+  //output to the file ”output.txt”.
   return 0;
 }
---
+++ Chapter1BasicsOfIO/Numbers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  //most commong numbers are int 32 bit numbers
+  int a = 4;
+  //is you need larger numbers use 64 bit numbers long long
+  long long b = 12321312312LL;
+  //We denote by x mod m the remainder when x is divided by m. For   example,
+  //17 mod 5 = 2, because 17 = 3·5+2.
+
+  
+
+  return 0;
+}
