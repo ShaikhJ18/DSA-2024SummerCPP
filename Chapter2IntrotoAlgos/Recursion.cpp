f4d7c2f838fac180d54dd90b4df64f50e2886ba2
Jawad Shaikh <J.shk1980@gmail.com>
2024-07-24 21:14:29 +0000

    added more info about complexity classes

---
+++ Chapter2IntrotoAlgos/Recursion.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+void f(int n) {
+if (n == 1) return;
+f(n-1);
+}
+// this function The call f(n) causes n function calls, and the time complexity of each call is O(1).
+//Thus, the total time complexity is O(n)
+
+int main(){
+
+
+  
+}
